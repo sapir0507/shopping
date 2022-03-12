6f0089e9c40dce7b8a28d8193658accd61159d60
sapir0507 <sapir.s@info.cts.org.il>
2022-03-12 12:27:41 +0200

    bootstrap added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'shopping'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shopping');
+    
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss', '../../node_modules/bootstrap/dist/css/bootstrap.min.css']
 })
 export class AppComponent {
-  title = 'shopping';
 }
