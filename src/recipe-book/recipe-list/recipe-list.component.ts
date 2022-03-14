25e3203e2f264b50a556a9b5c88aadf9867ad9b0
sapir0507 <sapir.s@info.cts.org.il>
2022-03-14 10:21:09 +0200

    import module

--- src/recipe-book/recipe-list/recipe-list.component.ts
+++ src/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('test recipe','this is simply a test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
+  ]
 
   constructor() { }
 
