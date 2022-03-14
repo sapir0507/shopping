2d974ec01a6b939a68d0ab9224247dfa441da940
sapir0507 <sapir.s@info.cts.org.il>
2022-03-14 13:07:13 +0200

    styling the recipes

--- src/recipe-book/recipe-list/recipe-list.component.ts
+++ src/recipe-book/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.module';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('test recipe','this is simply a test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
+    new Recipe('test recipe 1','this is simply a test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'),
+    new Recipe('test recipe 2','this is simply a test','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
   ]
 
   constructor() { }
