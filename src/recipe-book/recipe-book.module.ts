0b4bb10ae87affc4349c9ea49e75459c41e24d48
sapir0507 <sapir.s@info.cts.org.il>
2022-03-14 10:44:25 +0200

    changed dirs

--- src/recipe-book/recipe-book.module.ts
+++ src/recipe-book/recipe-book.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeBookComponent } from './recipe-book.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-details/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeRoutingModule } from './recipe-book-routing.module';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
